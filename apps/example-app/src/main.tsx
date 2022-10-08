55b5c42c449c23bff1c284f2329f0fe46ac9148a
rlmcneary2 <rlmcneary2@gmail.com>
2022-10-08 17:38:27 -0400

    Upgrade NX to 14.

--- apps/example-app/src/main.tsx
+++ apps/example-app/src/main.tsx
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./app/app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
