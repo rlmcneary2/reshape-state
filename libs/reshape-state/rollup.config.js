31e1d9435b7a6b24ba4af6f76d76c2433d447f7c
rlmcneary2 <rlmcneary2@gmail.com>
2022-11-13 13:28:24 -0500

    Updates to bundle building.

--- libs/reshape-state/rollup.config.js
+++ libs/reshape-state/rollup.config.js
@@ -1,10 +1,12 @@
 const nrwlConfig = require("@nrwl/react/plugins/bundle-rollup");
 
 module.exports = config => {
+  console.log("Rollup[libs/reshape-state/rollup.config.js]: enter.");
   const nextConfig = nrwlConfig(config);
 
   nextConfig.output = nextConfig.output ?? {};
   nextConfig.output.sourcemap = true;
 
+  console.log("Rollup[libs/reshape-state/rollup.config.js]: exit.");
   return nextConfig;
 };
