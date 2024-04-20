1db8ddeea81f5c1e23c1373260fd7d9b60f36d4e
rlmcneary2 <rlmcneary2@gmail.com>
2024-04-20 16:56:10 -0400

    NX upgrade from 14 to 15.

--- apps/example-app/jest.config.ts
+++ apps/example-app/jest.config.ts
@@ -3,10 +3,10 @@ export default {
   preset: "../../jest.preset.js",
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
-    "^.+\\.[tj]sx?$": "ts-jest"
+    "^.+\\.[tj]sx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
   coverageDirectory: "../../coverage/apps/example-app",
-  globals: { "ts-jest": { tsconfig: "<rootDir>/tsconfig.spec.json" } },
+  globals: {},
   displayName: "example-app"
 };
